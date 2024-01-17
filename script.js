55762f2c4b247c5d388796952b2e4ef5eb1b072a
HuzareX <danielwegierski@gmail.com>
2024-01-17 19:58:56 +0100

    Added new functions: renderCart, increaseAmount, decreaseAmount, deleteAmount

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ closeButton.addEventListener('click', closeCart)
 
 //RenderProducts
 const sectionProducts = document.querySelector('.products')
+const cartContainer = document.querySelector('.cart__container')
 const cart = []
 const products = [
 	{
@@ -77,28 +78,136 @@ const renderProducts = () => {
 		productWrapper.appendChild(productName)
 		productWrapper.appendChild(productPrice)
 		productWrapper.appendChild(addBtn)
-		dupa = productWrapper
 		sectionProducts.appendChild(productWrapper)
 	})
 }
 
 renderProducts()
 
-
 const addButtons = document.querySelectorAll('.add-btn')
 
+const addToCart = (productName, productPrice) => {
+	console.log(`Added a ${productName} to your cart at a price ${productPrice} PLN`);
 
-const showProductInfo = (productName, productPrice) => {
-	console.log(`Produkt: ${productName}, Cena: ${productPrice}`);
+	const existingProduct = cart.find(product => product.name === productName)
+
+	if (existingProduct) {
+		existingProduct.amount += 1
+		existingProduct.total += existingProduct.price
+	} else {
+		const newProduct = {
+			name: productName,
+			total: productPrice,
+			price: productPrice,
+			amount: 1,
+		}
+
+		cart.push(newProduct)
+	}
+
+	console.log(cart)
+	renderCart()
 }
+
+const increaseAmount = productName => {
+	const existingProduct = cart.find(product => product.name === productName)
+
+	if (existingProduct) {
+		existingProduct.amount += 1
+		existingProduct.total += existingProduct.price
+	} 
+
+	renderCart()
+}
+const decreaseAmount = productName => {
+	const existingProduct = cart.find(product => product.name === productName)
+
+	if (existingProduct.amount > 1) {
+		existingProduct.amount -= 1
+		existingProduct.total -= existingProduct.price
+	} else {
+		cart.splice(existingProduct, 1)
+	}
+
+	renderCart()
+}
+const deleteAmount = productName => {
+	const existingProduct = cart.find(product => product.name === productName)
+
+	if (existingProduct !== -1) {
+		cart.splice(existingProduct, 1)
+		
+	} 
+
+	renderCart()
+}
+
+
+
+
+
+const renderCart = () => {
+
+	cartContainer.innerHTML = ''
+
+	cart.forEach(product => {
+		const newItem = document.createElement('div')
+		newItem.classList.add('cart__item')
+
+		const itemName = document.createElement('p')
+		itemName.classList.add('cart__item-name')
+		itemName.textContent = product.name
+
+		const itemAmount = document.createElement('p')
+		itemAmount.classList.add('cart__item-amount')
+		itemAmount.textContent = `Amount: ${product.amount}`
+
+		const itemPrice = document.createElement('p')
+		itemPrice.classList.add('cart__item-price')
+		itemPrice.textContent = `Price: ${product.total.toFixed(2)} PLN`
+
+		const increaseBtn = document.createElement('button')
+		increaseBtn.classList.add('increase-button', 'cart-button')
+		const faPlus = document.createElement('i')
+		faPlus.classList.add('fa-solid', 'fa-plus')
+		increaseBtn.appendChild(faPlus)
+		increaseBtn.addEventListener('click', () => increaseAmount(product.name))
+
+		const decreaseBtn = document.createElement('button')
+		decreaseBtn.classList.add('decrease-button', 'cart-button')
+		const faMinus = document.createElement('i')
+		faMinus.classList.add('fa-solid', 'fa-minus')
+		decreaseBtn.appendChild(faMinus)
+		decreaseBtn.addEventListener('click', () => decreaseAmount(product.name))
+
+		const deleteBtn = document.createElement('button')
+		deleteBtn.classList.add('delete-button', 'cart-button')
+		const faXmark = document.createElement('i')
+		faXmark.classList.add('fa-solid', 'fa-xmark')
+		deleteBtn.appendChild(faXmark)
+		deleteBtn.addEventListener('click', () => deleteAmount(product.name))
+
+
+		cartContainer.appendChild(newItem)
+		newItem.appendChild(itemName)
+		newItem.appendChild(itemAmount)
+		newItem.appendChild(itemPrice)
+		newItem.appendChild(increaseBtn)
+		newItem.appendChild(decreaseBtn)
+		newItem.appendChild(deleteBtn)
+	})
+
+}
+
+
+
+
 
 addButtons.forEach((addBtn, index) => {
 	addBtn.addEventListener('click', () => {
 		const selectedProduct = products[index]
 		const productName = selectedProduct.name
 		const productPrice = selectedProduct.price
-
-		showProductInfo(productName,productPrice)
+		addToCart(productName, productPrice)
 	})
 })
-
